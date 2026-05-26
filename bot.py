09f05f6d423a85a396d57bb3cc35278f3d7c4280
hkh41912450-png <hkh41912450@gmail.com>
2026-05-27 00:46:58 +0800

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
-BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
+BOT_TOKEN = ***"DISCORD_BOT_TOKEN"]
 
 intents = discord.Intents.default()
 intents.message_content = True
